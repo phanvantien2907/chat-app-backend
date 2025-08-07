4354379d9aea4d81da3b2a1313a5570e1f79ea92
phanvantien2907 <vantienp530@gmail.com>
2025-08-07 14:03:49 +0700

    fix bug CROSS

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { CatchEverythingFilter } from 'src/exception/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({
-    origin: `${process.env.FRONTEND_URL}`, // Cho phép tất cả các nguồn gốc
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức HTTP được phép
-    allowedHeaders: 'Content-Type, Authorization', // Các tiêu đề được phép
-    credentials: true, // Cho phép cookie được gửi trong các yêu cầu cross-origin
-  });
   app.setGlobalPrefix('api'); // Thiết lập tiền tố toàn cục cho các route
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Loại bỏ các thuộc tính không được định nghĩa trong DTO
